9d5e081a114d805a0d65b85bac423ca1b2793374
Paulo Almeida <paulloroger@gmail.com>
2023-08-19 16:05:06 -0300

    Exemplos

---
+++ facebook_maen_media.R
@@ -0,0 +1,20 @@
+facebook
+
+mean(facebook$like, 
+     na.rm = TRUE
+     
+     )
+
+median(facebook$like, 
+     na.rm = TRUE
+     
+)
+
+boxplot(facebook$like)
+    
+
+summary(facebook$like)
+
+sd(facebook$like, 
+     na.rm = TRUE)
+
--- index_operadores_logicos.R
+++ index_operadores_logicos.R
@@ -1,6 +1,15 @@
+# criar objeto poisson
+poissonSimulacao <- rpois(
+  n=50,
+  lambda=2
+  )
 # vetores
 poissonSimulacao[1] # acessa o primeiro elemento
-poissonSimulacao[c(1:10, 15)] # acessa os elementos 1, 2 até 10 e 15
+poissonSimulacao[c(
+  1:10,
+  15
+  )
+  ] # acessa os elementos 1, 2 até 10 e 15
 
 # matrizes
 matrix1[1, ] # linha 1
@@ -9,6 +18,7 @@ matrix1[1,1] # linha 1, coluna 1
 
 # data.frames
 iris$Species # retorna apenas a coluna species do data.frame iris
+iris$Sepal.Length
 
 iris[ , 5] # retorna todas as linhas apenas a coluna species do data.frame iris
 
@@ -49,7 +59,8 @@ all(a != c, a < c, a == c)
 # operadores lógicos na prática
 iris$Sepal.Length <= 0 # testa se os valores na sepal.length são menores ou iguais a 0 
 
-iris$Sepal.Length >= 0 & iris$Sepal.Width <= 0.2 # testa se, numa dada linha, os valores na sepal.length são menores ou iguais a 0 OU se os valores em sepal.width são iguais ou menores que 0.2
+iris$Sepal.Length >= 0 & 
+iris$Sepal.Width <= 0.2 # testa se, numa dada linha, os valores na sepal.length são menores ou iguais a 0 OU se os valores em sepal.width são iguais ou menores que 0.2
 
 which(iris$Sepal.Length <= 5) # a função which mostra a posição (as linhas) em que a condição é atendida
 
